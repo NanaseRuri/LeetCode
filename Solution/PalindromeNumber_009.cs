26170cca5f96c2068fadbba649c2400be92ad2c9
NanaseRuri <980498809@qq.com>
2019-09-13 11:46:56 +0800

    9. 回文数

---
+++ Solution/PalindromeNumber_009.cs
@@ -0,0 +1,22 @@
+namespace LeetCode.Solution
+{
+    public class PalindromeNumber_009
+    {
+        public bool IsPalindrome(int x)
+        {
+            int origin=x;
+            int result = 0;
+            if (x < 0)
+            {
+                return false;
+            }
+            while (x != 0)
+            {
+                int temp=x%10;
+                result=10*result+temp;
+                x/=10;                
+            }
+            return result==origin;
+        }
+    }
+}
